5a9b38f602263a9a7265a22704556c9945806a8d
GloriaAtieno <atienogloria07@gmail.com>
2024-11-07 10:16:46 +0300

    Django project

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static_files' # main directory - created automatically
+
+STATICFILES_DIRS = [BASE_DIR / 'main_app/static'] # each app in the project
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
